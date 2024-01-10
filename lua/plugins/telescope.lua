e9f91847669eeff2d8aead80c84608d97ba7ddea
Philipp Viereck <philipp@mailbean.io>
2024-01-10 17:18:54 +0100

    make telescope startscreen

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,13 +1,20 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.4",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			local builtin = require("telescope.builtin")
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.4",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local builtin = require("telescope.builtin")
 
-			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-		end,
-	},
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+      vim.api.nvim_create_autocmd("VimEnter", {
+        callback = function()
+          if vim.fn.argv(0) == "" then
+            require("telescope.builtin").find_files()
+          end
+        end,
+      })
+    end,
+  },
 }
