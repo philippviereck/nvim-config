2bb578fb56a0fe7f287c33de4ce961c19f20ab22
Philipp Viereck <philipp@mailbean.io>
2024-01-12 11:37:31 +0100

    add mappings to cycle buffers

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -115,13 +115,13 @@ local function ins_right(component)
   table.insert(config.sections.lualine_x, component)
 end
 
-ins_left {
-  function()
-    return '▊'
-  end,
-  color = { fg = colors.blue }, -- Sets highlighting of component
-  padding = { left = 0, right = 1 }, -- We don't need space before this
-}
+-- ins_left {
+--   function()
+--     return '▊'
+--   end,
+--   color = { fg = colors.blue }, -- Sets highlighting of component
+--   padding = { left = 0, right = 1 }, -- We don't need space before this
+-- }
 
 ins_left {
   -- mode component
@@ -257,14 +257,14 @@ ins_right {
   cond = conditions.hide_in_width,
 }
 
-ins_right {
-  function()
-    return '▊'
-  end,
-  color = { fg = colors.blue },
-  padding = { left = 1 },
-}
-
+-- ins_right {
+--   function()
+--     return '▊'
+--   end,
+--   color = { fg = colors.blue },
+--   padding = { left = 1 },
+-- }
+--
 -- Now don't forget to initialize lualine
 lualine.setup(config)
   end,
